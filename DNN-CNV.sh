a25dfa4f61cee3f92a438fcf916e89bdf1e240af
Brett <brett.trost@sickkids.ca>
2018-06-10 15:31:21 -0400

    Added part of functionality for using read-depth features

--- DNN-CNV.sh
+++ DNN-CNV.sh
@@ -3,6 +3,15 @@
 ERDS_filename=$1
 CNVnator_filename=$2
 job_ID=$3
+BAM_file=$4
+
+if [[ $BAM_file ]]; then
+	depth_option="--depth-filename $BAM_file.depth"
+	model_name="ABCD"
+else
+	depth_option=""
+	model_name="ABD"
+fi
 
 print_status "Converting ERDS file to common format...\n"
 convert_CNV_calls_to_common_format.py $ERDS_filename ERDS > $job_ID.ERDS.common.txt
@@ -14,7 +23,7 @@ print_status "Merging ERDS and CNVnator files...\n"
 CNV_overlap.py -f Zhuozhi $job_ID.ERDS.common.txt $job_ID.CNVnator.common.txt > $job_ID.merged.txt
 
 print_status "Generating features from merged file...\n"
-generate_features.py --repeat-filename $RLCRS_DIR/RLCRs_DNN-CNV.txt $job_ID.merged.txt
+generate_features.py $depth_option --repeat-filename $RLCRS_DIR/RLCRs_DNN-CNV.txt $job_ID.merged.txt
 
 print_status "Generating predictions...\n"
-run_model.py --omit-truth $job_ID.merged.txt.features /hpf/largeprojects/tcagstor/users/btrost/papers/DNN-CNV/model_evaluation/NA12878/NA12878.txt.bm.RLCR.dirty.annotated2.features.DNN-CNV/cv_models/NA12878.txt.bm.RLCR.dirty.annotated2.features.all.model.meta
+run_model.py --omit-truth $job_ID.merged.txt.features all.features.$model_name.DNN-CNV/models/all.features.$model_name.all.model.meta
