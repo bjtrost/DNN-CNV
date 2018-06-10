eb1c3bbf5ff89a8804bc08c76cb1fe600edadfff
Brett <brett.trost@sickkids.ca>
2018-06-10 15:41:28 -0400

    No subdirectory for RLCR files

--- DNN-CNV.sh
+++ DNN-CNV.sh
@@ -8,6 +8,13 @@ BAM_file=$4
 if [[ $BAM_file ]]; then
 	depth_option="--depth-filename $BAM_file.depth"
 	model_name="ABCD"
+	if [ ! -e $BAM_file.depth ]; then
+    		samtools depth $BAM_file > $BAM_file.depth
+	fi
+
+	if [ ! -e $BAM_file.depth.idx ]; then
+		index_samtools_depth.py $BAM_file.depth
+	fi
 else
 	depth_option=""
 	model_name="ABD"
@@ -23,7 +30,7 @@ print_status "Merging ERDS and CNVnator files...\n"
 CNV_overlap.py -f Zhuozhi $job_ID.ERDS.common.txt $job_ID.CNVnator.common.txt > $job_ID.merged.txt
 
 print_status "Generating features from merged file...\n"
-generate_features.py $depth_option --repeat-filename $RLCRS_DIR/RLCRs_DNN-CNV.txt $job_ID.merged.txt
+generate_features.py $depth_option --repeat-filename RLCRs_DNN-CNV.txt $job_ID.merged.txt
 
 print_status "Generating predictions...\n"
 run_model.py --omit-truth $job_ID.merged.txt.features all.features.$model_name.DNN-CNV/models/all.features.$model_name.all.model.meta
